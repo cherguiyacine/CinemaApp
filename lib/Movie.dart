c5c049ab584835c7fd27992cc7d4114117c5492c
yacine chergui <hb_chergui@esi.dz>
2020-04-03 22:41:15 +0200

    add ratingBar Widget  to MovieDescription Page

--- lib/Movie.dart
+++ lib/Movie.dart
@@ -1,7 +1,7 @@
 class Movie {
   String name = "MAD MAX";
   String time = "1h 34mn";
-  String mark = "4.5";
+  double mark = 4.5;
   String synopsis =
       "Taking place in a dystopian Australia in the near future, Mad Max tells the story of a highway patrolman cruising the squalid back roads that have become the breeding ground of criminals foraging for gasoline and scraps";
 }
